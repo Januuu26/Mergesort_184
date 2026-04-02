37bedac5a0703bfaef4b3a73698d009386f7d41e
Januuu26 <b77286116@gmail.com>
2026-04-02 18:14:08 +0700

    membuat batas panjang arrray

--- mergesort.cpp
+++ mergesort.cpp
@@ -13,6 +13,13 @@ void input()
         cout << "Masukan panjang element array : ";
         cin >> n;
 
-        
+        if (n <= 20)
+        {
+            break;   
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
     }
 }
